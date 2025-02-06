d5614cfa36dc19f742aace5e7734dae016ef3151
Glowbig <mecatronica@iflexo.com.co>
2025-02-06 11:02:13 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Adrian Estrada",                         # Tu nombre
+    author_email="ader.adrianestradarios@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Glowbig/tercerRepo",     # URL del proyecto
+)
